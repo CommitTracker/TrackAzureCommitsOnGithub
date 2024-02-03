d10a0b9fa210885dcccb013b38480e78365f68d4
Matthew Rawson <matthewrawson2@gmail.com>
2024-02-03 01:14:19 +0000

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -10,6 +10,18 @@ if "%pyver%"=="3.11.7" (
     exit /b 1
 )
 
+:: Check for default values in appsettings.json
+set "defaultsFound=0"
+findstr /c:"\"PAT\": \"your_personal_access_token_here\"" appsettings.json > nul && (echo Make sure PAT is updated from the default value. && set "defaultsFound=1")
+findstr /c:"\"Organization\": \"organization\"" appsettings.json > nul && (echo Make sure Organization is updated from the default value. && set "defaultsFound=1")
+findstr /c:"\"Project\": \"project\"" appsettings.json > nul && (echo Make sure Project is updated from the default value. && set "defaultsFound=1")
+findstr /c:"\"Repo\": \"repo\"" appsettings.json > nul && (echo Make sure Repo is updated from the default value. && set "defaultsFound=1")
+
+if "%defaultsFound%"=="1" (
+    echo One or more default values are not updated in appsettings.json. Please update them before proceeding.
+    exit /b 1
+)
+
 :: Install GitPython
 echo Installing GitPython...
 python -m pip install GitPython
